d99701494a53f73c01c3a59457e9dbf0e68e20c3
WOWmasterJulia <topazzz@ukr.net>
2023-08-31 17:32:50 +0300

    Changes

--- App.js
+++ App.js
@@ -14,42 +14,70 @@ import Comments from "./src/components/CommentsScreen";
 import CreatePostsScreen from "./src/components/CreatePostsScreen";
 import Map from "./src/components/MapScreen";
 
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import rootReducer from "./rootReducer";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+};
+const reducer = persistReducer(persistConfig, rootReducer);
+const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+const persistor = persistStore(store);
 const MainStack = createStackNavigator(); // вказує на групу навігаторів
-
 export default function App() {
   // const [fontsLoaded] = useFonts({
-  //   "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
-  //   "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+  //   "Roboto-Regular": require("./assets_new/fonts/Roboto-Regular.ttf"),
+  //   "Roboto-Medium": require("./assets_new/fonts/Roboto-Medium.ttf"),
   // });
   // if (!fontsLoaded) {
   //   return null;
   // }
   return (
-    <NavigationContainer>
-      <MainStack.Navigator
-        initialRouteName="Login"
-        screenOptions={{ headerShown: false }}
-      >
-        <MainStack.Screen name="Registration" component={Registration} />
-        <MainStack.Screen name="Login" component={Login} />
-        {/* <MainStack.Screen name="PhotoCamera" component={PhotoCamera} /> */}
-        {/* <MainStack.Screen name="Post" component={Post} /> */}
-        <MainStack.Screen name="Home" component={Home} />
-        <MainStack.Screen name="Comments" component={Comments} />
-        <MainStack.Screen name="createPostsScreen" component={CreatePostsScreen} />
-        <MainStack.Screen name="Map" component={Map} />
-      </MainStack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <MainStack.Navigator
+            initialRouteName="Login"
+            screenOptions={{ headerShown: false }}
+          >
+            <MainStack.Screen name="Registration" component={Registration} />
+            <MainStack.Screen name="Login" component={Login} />
+            {/* <MainStack.Screen name="Post" component={Post} /> */}
+            <MainStack.Screen name="Home" component={Home} />
+            <MainStack.Screen name="Comments" component={Comments} />
+            <MainStack.Screen
+              name="createPostsScreen"
+              component={CreatePostsScreen}
+            />
+            <MainStack.Screen name="Map" component={Map} />
+          </MainStack.Navigator>
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 }
+// a-project-native-1
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     // alignItems: 'center',
-//     // justifyContent: 'center',
-//     //
 
-//     // paddingBottom: 30,
-//   },
-// });
+
---
+++ config.js
@@ -0,0 +1,57 @@
+// Для роботи із firebase обовʼязково треба ініціалізувати проект
+import { initializeApp } from "firebase/app";
+// Функція для підключення авторизації в проект
+import { getAuth } from "firebase/auth";
+// Функція для підключення бази даних у проект
+import { getFirestore } from "firebase/firestore";
+// Функція для підключення сховища файлів в проект
+import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBLcnU0RdW7H1JjYHmmhmUOlnB2iNDVqSk",
+  authDomain: "a-project-native-1.firebaseapp.com",
+  projectId: "a-project-native-1",
+  storageBucket: "a-project-native-1.appspot.com",
+  messagingSenderId: "961054400783",
+  appId: "1:961054400783:web:d42f43ab4531d3ad9c5e7a",
+  measurementId: "G-QY3R9TRTTY",
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+
+const { getDefaultConfig } = require("@expo/metro-config");
+
+const defaultConfig = getDefaultConfig(__dirname);
+defaultConfig.resolver.assetExts.push("cjs");
+
+module.exports = defaultConfig;
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBLcnU0RdW7H1JjYHmmhmUOlnB2iNDVqSk",
+//   authDomain: "a-project-native-1.firebaseapp.com",
+//   projectId: "a-project-native-1",
+//   storageBucket: "a-project-native-1.appspot.com",
+//   messagingSenderId: "961054400783",
+//   appId: "1:961054400783:web:d42f43ab4531d3ad9c5e7a",
+//   measurementId: "G-QY3R9TRTTY"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
